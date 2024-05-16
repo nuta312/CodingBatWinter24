779c8cb7582b3a830b4cc5e8191485204aee0b55
Dzhamilya <sherimbekovadjama@gmail.com>
2024-05-16 17:19:46 +0600

    Task/HelloName

---
+++ src/main/java/com/digital_nomads/dzhama/string1/HelloName.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.dzhama.string1;
+
+public class HelloName {
+    public String helloName(String name) {
+        return "Hello" + name + "!";
+    }
+}
---
+++ src/main/java/com/digital_nomads/dzhama/warmup1/SleeIn.java
@@ -0,0 +1,5 @@
+package com.digital_nomads.dzhama.warmup1;
+
+public class SleeIn {
+    public
+}
