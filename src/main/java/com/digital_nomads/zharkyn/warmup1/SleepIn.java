d1bbf78652a398e6baf34de64be049a9fdae0c3d
Zharkyn <zharkyn.akatova1987@gmail.com>
2024-05-16 21:29:37 +0600

    test:SleepIn

--- src/main/java/com/digital_nomads/zharkyn/warmup1/SleepIn.java
+++ src/main/java/com/digital_nomads/zharkyn/warmup1/SleepIn.java
@@ -5,7 +5,7 @@ public class SleepIn {
     public boolean sleepIn(boolean weekday, boolean vacation) {
         if (!weekday || vacation) {
             return true;
-        } return false;
+        }   return false;
     }
 
 }
--- src/test/java/zharkyn/warmup1_test/SleepInTest.java
+++ src/test/java/zharkyn/warmup1_test/SleepInTest.java
@@ -1,5 +1,26 @@
 package zharkyn.warmup1_test;
 
+import com.digital_nomads.zharkyn.warmup1.SleepIn;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class SleepInTest {
+
+    @Test (dataProvider = "sleepInDataProvider")
+    public void sleepInTest(boolean weekday, boolean vacation, boolean expected){
+        SleepIn sleepIn = new SleepIn();
+        Assert.assertEquals(sleepIn.sleepIn(weekday, vacation), expected);
+    }
+
+    @DataProvider (name = "sleepInDataProvider")
+    public Object[][] sleepInDataProvider(){
+        return new Object [][]{
+            {false, false, true},
+            {true, false, false},
+            {false, true, true},
+            {true, true, true}
+        };
+    }
 
 }
