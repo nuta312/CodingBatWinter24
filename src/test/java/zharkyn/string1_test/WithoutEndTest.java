0a5e3ea6a968db3fe144d7900c760165745d55bb
Zharkyn <zharkyn.akatova1987@gmail.com>
2024-05-17 00:17:29 +0600

    test: ComboString

--- src/test/java/zharkyn/string1_test/WithoutEndTest.java
+++ src/test/java/zharkyn/string1_test/WithoutEndTest.java
@@ -1,16 +1,17 @@
 package zharkyn.string1_test;
 
 import com.digital_nomads.zharkyn.string1.WithoutEnd;
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class WithoutEndTest {
 
     @Test (dataProvider = "withoutEndDataProvider")
     public void withoutEndTest(String str, String expected){
         WithoutEnd withoutEnd = new WithoutEnd();
-        Assert.assertEquals(withoutEnd.withoutEnd(str), expected);
+        assertEquals(withoutEnd.withoutEnd(str), expected);
     }
 
     @DataProvider(name = "withoutEndDataProvider")
