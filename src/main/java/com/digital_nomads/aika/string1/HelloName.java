f6f022bfa3c40b7e5ef715f8767609c75c389e78
Aikanysh <mukanbet1986@gmail.com>
2024-05-16 16:41:03 +0600

    Test: aika HelloNameTest: протестировала через DataProvider

--- src/main/java/com/digital_nomads/aika/string1/HelloName.java
+++ src/main/java/com/digital_nomads/aika/string1/HelloName.java
@@ -3,6 +3,6 @@ package com.digital_nomads.aika.string1;
 public class HelloName {
 
     public String helloName(String name){
-        return "Hello" + name+ "!";
+        return "Hello " + name+ "!";
     }
 }
