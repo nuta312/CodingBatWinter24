61cf01df73cb71ea9446591ce0e30e52908c83b7
eldar <borubaev.eldar15@gmail.com>
2024-05-16 20:08:25 +0600

    EldarSTR Commit

---
+++ src/main/java/com/digital_nomads/eldar/str1/_1helloName.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.eldar.str1;
+
+public class _1helloName {
+    public String heloName (String str){
+        return "Hello " + str + "!";
+    }
+
+}
---
+++ src/test/java/eldar_test/str1/test/_2MakeABBA.java
@@ -0,0 +1,27 @@
+package eldar_test.str1.test;
+
+import com.digital_nomads.eldar.str1.MakeABBA;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class _2MakeABBA {
+
+    @Test (dataProvider = "MABBAT")
+    public void testABBA(String a, String b, String e) {
+        MakeABBA abba = new MakeABBA();
+        String res = abba.makeAbba(a,b);
+        assert res.equals(e);
+
+    }
+
+    @DataProvider
+    public Object[][] MABBAT() {
+        return new Object[][]{
+                {"Hi", "Bye", "HiByeByeHi"},
+                {"Yo","Alice","YoAliceAliceYo"},
+                {"What","Up","WhatUpUpWhat"},
+                {"phone","Apple", "phoneAppleApplephone"},
+        };
+
+    }
+}
