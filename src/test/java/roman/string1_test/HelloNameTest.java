bed132b8952a080a448ece809438a4e9da328196
roman <appozit@mail.ru>
2024-05-16 23:52:04 +0600

    roman:helloname

---
+++ src/test/java/roman/string1_test/HelloNameTest.java
@@ -0,0 +1,14 @@
+package roman.string1_test;
+
+import com.digital_nomads.roman.string1.HelloName;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HelloNameTest {
+    @Test
+    public void testHelloName(){
+        HelloName helloName = new HelloName();
+        Assert.assertEquals(helloName.helloName("Bob"),"Hello Bob!");
+
+    }
+}
