f3e9e5bef98ee764e87fb3ae221e090e4f6394b3
Nargiza <nargizaknd210986@gmail.com>
2024-05-16 23:58:45 +0600

    test:FirstHalf

---
+++ src/main/java/com/digital_nomads/nargiza/string1/FirstHalf.java
@@ -0,0 +1,9 @@
+package com.digital_nomads.nargiza.string1;
+
+public class FirstHalf {
+
+    public String firstHalf(String str) {
+        int length1 = str.length() / 2;
+        return str.substring(0, length1);
+    }
+}
