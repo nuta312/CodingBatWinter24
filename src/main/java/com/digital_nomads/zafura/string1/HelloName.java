2001add3a49ef8f377f61570718058520089c012
zafura <zafura25@gmail.com>
2024-05-17 18:30:23 +0300

    makebba-test

---
+++ src/main/java/com/digital_nomads/zafura/string1/HelloName.java
@@ -0,0 +1,10 @@
+package com.digital_nomads.zafura.string1;
+
+public class HelloName {
+    public String helloName(String name) {
+
+        return "Hello " + name + "!";
+
+    }
+
+}
---
+++ src/test/java/zafuraTest/string1_test/MakeAbbaTest.java
@@ -0,0 +1,12 @@
+package zafuraTest.string1_test;
+
+import com.digital_nomads.zafura.string1.MakeAbba;
+import org.testng.annotations.Test;
+
+public class MakeAbbaTest {
+    @Test
+    public void makeAbbaTest(){
+        MakeAbba makeAbba = new MakeAbba();
+
+    }
+}
