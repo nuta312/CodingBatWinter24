36ab9554b22bf4c8f0713709322fda8c2db2b999
hakerWhite <sunnatravel.kg2022@gmail.com>
2024-05-17 21:22:29 +0300

    aiba/SameEnds

--- src/test/java/aiba/string1_test/HelloNameTest.java
+++ src/test/java/aiba/string1_test/HelloNameTest.java
@@ -1,13 +1,4 @@
 package aiba.string1_test;
 
-import com.digital_nomads.aiba.string1.HelloName;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 public class HelloNameTest {
-    @Test
-    public void testHelloName(){
-        HelloName helloName = new HelloName();
-        Assert.assertEquals(helloName.helloName("Bob"),"Hello Bob!");
-    }
 }
