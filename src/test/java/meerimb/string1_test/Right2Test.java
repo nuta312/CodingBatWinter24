905db5b9091988658d452a7451bf52649fa90ae3
MeerimB <meerimb312@gmail.com>
2024-05-17 12:42:06 +0300

    meerimb/String1/right2

---
+++ src/test/java/meerimb/string1_test/Right2Test.java
@@ -0,0 +1,28 @@
+package meerimb.string1_test;
+
+import com.digital_nomads.meerimb.string1.Right2;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Right2Test {
+    Right2 right2 = new Right2();
+
+    @Test(dataProvider = "right2")
+    public void testRight2(String str, String expected) {
+
+        Assert.assertEquals(right2.right2(str), expected);
+    }
+
+    @DataProvider
+    public Object[][] right2() {
+        return new Object[][]{
+                {"Hello", "loHel"},
+                {"java", "vaja"},
+                {"Hi", "Hi"},
+                {"code", "deco"},
+                {"cat", "atc"},
+                {"12345", "45123"},
+        };
+    }
+}
