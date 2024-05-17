631e9018530d00eb89dfcd6f3157800a7877c443
Aikanysh <mukanbet1986@gmail.com>
2024-05-17 23:15:42 +0600

    aika/Warmup/SleepIn

--- src/main/java/com/digital_nomads/aika/warmup1/SleepIn.java
+++ src/main/java/com/digital_nomads/aika/warmup1/SleepIn.java
@@ -1,7 +1,11 @@
 package com.digital_nomads.aika.warmup1;
 
 public class SleepIn {
-    //public boolean sleepIn(boolean weekday, boolean vacation) {
-
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (weekday && !vacation) {
+            return false;
+        } else {
+            return true;
+        }
     }
-
+}
