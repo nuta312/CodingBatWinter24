f7ec3af9f581377c8136f5c96b936935a550a7c5
Dzhamilya <sherimbekovadjama@gmail.com>
2024-05-18 22:30:30 +0600

    String1MakeTags

---
+++ src/main/java/com/digital_nomads/dzhamilya/string1/MakeTags.java
@@ -0,0 +1,7 @@
+package com.digital_nomads.dzhamilya.string1;
+
+public class MakeTags {
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+}
---
+++ src/test/java/dzhamilya/string1_test/MakeTagsTest.java
@@ -0,0 +1,32 @@
+package dzhamilya.string1_test;
+
+import com.digital_nomads.dzhamilya.string1.MakeTags;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class MakeTagsTest {
+
+    @Test
+            (dataProvider = "MakeTest")
+    public void TagsTest(String tag, String word, String expected) {
+        MakeTags tags = new MakeTags();
+        String result = tags.makeTags(tag, word);
+        Assert.assertEquals(result, expected);
+
+
+    }
+@DataProvider
+    public Object[][] MakeTest() {
+        return new Object[][] {
+                {"i", "Yay" , "<i>Yay</i>"},
+                {"i", "Hello", "<i>Hello</i>"},
+                {"cite", "Yay", "<cite>Yay</cite>"},
+                {"address", "here", "<address>here</address>"},
+                {"body", "Heart", "<body>Heart</body>"},
+                {"i", "i",  "<i>i</i>"},
+                {"i", "", "<i></i>"},
+
+        };
+    }
+}
