40df43000d0af98ca5434dbfe973e3cb31a38331
AisuluuKe <kemelovaaisuluu@gmail.com>
2024-05-18 19:33:31 +0600

    Aisuluu/HelloName

--- src/test/java/aisuluu/string1_test/HelloNameTest.java
+++ src/test/java/aisuluu/string1_test/HelloNameTest.java
@@ -1,5 +1,29 @@
 package aisuluu.string1_test;
 
-public class HelloNameTest {
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+public class HelloNameTest {
+    @Test(dataProvider = "HelloNameTest")
+    public void HelloNameTest(String name, String expected){
+        HelloNameTest helloNameTest = new HelloNameTest();
+        Assert.assertEquals(helloNameTest.HelloNameTest((name), expected);
+
+    }
+    @DataProvider
+    public Object[][] HelloNameTest() {
+        return new Object[][]{
+                {"Bob", "Hello Bob!"},
+                {"Alice", "Hello Alice!"},
+                {"X", "Hello X!"},
+                {"Dolly", "Hello Dolly!"},
+                {"Alpha", "Hello Alpha!"},
+                {"Omega", "Hello Omega!"},
+                {"Goodbye", "Hello Goodbye!"},
+                {"ho ho ho", "Hello ho ho ho!"},
+                {"xyz!", "Hello xyz!!"},
+                {"Hello", "Hello Hello!"}
+        };
+    }
 }
