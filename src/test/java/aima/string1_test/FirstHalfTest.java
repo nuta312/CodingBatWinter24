646b3d0a1c6a5a72bafd75f4d4cf77311735cd8f
aima <aykesha8787@gmail.com>
2024-05-18 12:06:56 +0600

    FirstHalfTest

---
+++ src/test/java/aima/string1_test/FirstHalfTest.java
@@ -0,0 +1,28 @@
+package aima.string1_test;
+
+import com.digital_nomads.aima.string1.FirstHalf;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class FirstHalfTest {
+
+    @Test (dataProvider = "words")
+    public void firstHalf(String str, String expected) {
+        FirstHalf firstHalf = new FirstHalf();
+        Assert.assertEquals(firstHalf.firstHalf(str), expected);
+
+    }
+    @DataProvider(name = "words")
+    public Object[][] providerWords() {
+        return new Object[][] {
+                {"WooHoo", "Woo"},
+                {"HelloThere", "Hello"},
+                {"abcdef", "abc"},
+                {"ab", "a"},
+                {"", ""},
+                {"0123456789", "01234"},
+                {"kitten", "kit"},
+        };
+}
+}
