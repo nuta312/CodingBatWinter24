66e100224a91477285068f663780abb8929dbe17
zafura <zafura25@gmail.com>
2024-05-19 22:57:55 +0300

    mskeoutword

--- src/test/java/zafuraTest/string1_test/MakeAbbaTest.java
+++ src/test/java/zafuraTest/string1_test/MakeAbbaTest.java
@@ -1,13 +1,27 @@
 package zafuraTest.string1_test;
 
 import com.digital_nomads.zafura.string1.MakeAbba;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class MakeAbbaTest {
-    @Test
-    public void makeAbbaTest(){
+    @Test(dataProvider = "makeAb")
+    public void makeAbbaTest(String a,String b, String expected){
         MakeAbba makeAbba = new MakeAbba();
+        Assert.assertEquals(makeAbba.makeAbba(a,b), expected);
+
 
 
     }
-}
+    @DataProvider
+    public Object[][]makeAb(){
+        return new Object[][]{
+                {"Hi", "Bye","HiByeByeHi"},
+                {"Yo", "Alice", "YoAliceAliceYo" },
+                {"What", "Up","WhatUpUpWhat"},
+                {"aaa", "bbb","aaabbbbbbaaa"},
+                {"x", "y","xyyx"},
+                {"x", "", "xx"},
+        };
+}}
