92bf41f559a7075274f84dd08bcb3642fd197a0f
MeerimB <meerimb312@gmail.com>
2024-05-19 10:19:49 +0300

    meerimb/String1/without2

---
+++ src/main/java/com/digital_nomads/meerimb/string1/Without2.java
@@ -0,0 +1,12 @@
+package com.digital_nomads.meerimb.string1;
+
+public class Without2 {
+
+    public String without2(String str) {
+        if (str.length() >= 2 &&
+                str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+            return str.substring(2);
+        }
+        return str;
+    }
+}
