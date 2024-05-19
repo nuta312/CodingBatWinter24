08da8cfaf4d428d24fd73a3be5e5ccd8a35dac30
ZHYLDYZ ADYLCHAEVA <asylbash@gmail.com>
2024-05-19 21:53:41 +0200

    Test: zhyldyz warmup1 nearHundred

---
+++ src/main/java/com/digital_nomads/zhyldyz/warmap1/NearHundred.java
@@ -0,0 +1,12 @@
+package com.digital_nomads.zhyldyz.warmap1;
+
+public class NearHundred {
+
+    public boolean nearHundred(int n) {
+        if (Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
