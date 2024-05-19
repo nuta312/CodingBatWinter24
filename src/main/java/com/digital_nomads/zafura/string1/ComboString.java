024d6093739489156c162193ae76e333234f2d8f
zafura <zafura25@gmail.com>
2024-05-19 23:38:51 +0300

    ComboString

---
+++ src/main/java/com/digital_nomads/zafura/string1/ComboString.java
@@ -0,0 +1,9 @@
+package com.digital_nomads.zafura.string1;
+
+public class ComboString {
+    public String comboString(String a, String b) {
+        return a.length()>b.length() ?   b+a+b : a+b+a ;
+
+    }
+
+}
