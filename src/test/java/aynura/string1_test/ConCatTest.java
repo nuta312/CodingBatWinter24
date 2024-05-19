4ccb388f577697365aaedb43c0eb420a3bc49f0d
aynura <ainurakojokmatova@gmail.com>
2024-05-19 16:45:41 +0600

    aynura/string1

---
+++ src/test/java/aynura/string1_test/ConCatTest.java
@@ -0,0 +1,28 @@
+package aynura.string1_test;
+
+import com.digital_nomads.aijan.string1.ConCat;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ConCatTest {
+
+    @DataProvider(name = "word")
+    public Object[][] provideWord() {
+        return new Object[][]{
+                {"abc", "cat","abcat"},
+                {"dog", "cat","dogcat"},
+                {"abc", "", "abc"},
+                {"", "cat", "cat"},
+                {"pig", "g", "pig"},
+                {"pig", "doggy","pigdoggy"}
+        };
+
+    }
+    @Test(dataProvider = "word")
+    public void testConCat(String a, String b, String expectedResult) {
+        ConCat obj = new ConCat();
+        String actualResult = obj.conCat(a,b);
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+}
