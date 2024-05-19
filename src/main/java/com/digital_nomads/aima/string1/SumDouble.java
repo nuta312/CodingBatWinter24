41de8abe34a77c3119be29ec8a306b9bb898ff5b
aima <aykesha8787@gmail.com>
2024-05-20 00:03:39 +0600

    SumDoubleTest

---
+++ src/main/java/com/digital_nomads/aima/string1/SumDouble.java
@@ -0,0 +1,11 @@
+package com.digital_nomads.aima.string1;
+
+public class SumDouble {
+    public int sumDouble(int a, int b) {
+        int sum = a+b;
+        if ( a==b) {
+            sum = sum*2;
+        }
+        return sum;
+    }
+}
--- src/test/java/aima/warmup1_test/MonkeyTroubleTest.java
+++ src/test/java/aima/warmup1_test/MonkeyTroubleTest.java
@@ -1,23 +1,28 @@
 package aima.warmup1_test;
 
 import com.digital_nomads.aima.warmup1.MonkeyTrouble;
-import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class MonkeyTroubleTest {
-@Test (dataProvider = "providerWords")
-    public void monkeyTrouble(boolean aSmile, boolean bSmile, boolean expected) {
     MonkeyTrouble monkeyTrouble = new MonkeyTrouble();
-    Assert.assertEquals(monkeyTrouble.monkeyTrouble(aSmile, bSmile), expected);
+
+    @DataProvider(name = "DataProvider")
+    public Object[][] monkeyTroubleDataProvider() {
+
+        return new Object[][]{
+
+                {true, true, true},
+                {false, false, true},
+                {true, false, false},
+                {false, true, false}
+        };
     }
-    @DataProvider (name = "providerWords")
-    public Object[][] providerWords(){
-    return new Object[][]{
-            {"true", "true", "true"},
-            {"false", "false", "true"},
-            {"true", "false", "false"},
-            {"false", "true", "false"},
-    };
+
+    @Test(dataProvider = "DataProvider")
+    void monkeyTroubleTest(boolean aSmile, boolean bSmile, boolean exception) {
+
+        assertEquals(monkeyTrouble.monkeyTrouble(aSmile, bSmile), exception);
     }
 }
