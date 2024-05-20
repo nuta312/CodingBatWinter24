84f16037884acbe9748c0a6f07dc60308f5fa8c1
Gaukhar <hayannahodeki@gmail.com>
2024-05-20 19:13:23 +0600

    FirstTwo

--- src/main/java/com/digital_nomads/gaukhar/string1/firstTwo.java
+++ src/main/java/com/digital_nomads/gaukhar/string1/firstTwo.java
@@ -1,2 +1,9 @@
-package com.digital_nomads.gaukhar.string1;public class firstTwo {
+package com.digital_nomads.gaukhar.string1;
+
+public class firstTwo {
+    public String firstTwo(String str) {
+        if (str.length() >2){
+            return str.substring(0,2);
+        }return str;
+    }
 }
