5767b7be86941cb2c6716fb4ed39b36cecefe5c2
Nargiza <nargizaknd210986@gmail.com>
2024-05-20 23:44:17 +0600

    test:Diff21

---
+++ src/main/java/com/digital_nomads/nargiza/warmup1/Diff21.java
@@ -0,0 +1,11 @@
+package com.digital_nomads.nargiza.warmup1;
+
+public class Diff21 {
+
+    public int diff21(int n) {
+        if (n<=21) {
+            return 21 -n;
+        } else
+            return (n-21)*2;
+    }
+}
