2017ad9f819181786c6620dcc2b35939a2636cbd
Gaukhar <hayannahodeki@gmail.com>
2024-05-20 18:57:03 +0600

    ExtraEnd

--- src/main/java/com/digital_nomads/gaukhar/string1/extraEnd.java
+++ src/main/java/com/digital_nomads/gaukhar/string1/extraEnd.java
@@ -1,2 +1,10 @@
-package com.digital_nomads.gaukhar.string1;public class extraEnd {
+package com.digital_nomads.gaukhar.string1;
+
+public class extraEnd {
+    public String extraEnd(String str) {
+        String n = str.substring(str.length()-2);
+        return n+n+n;
+    }
+
+
 }
