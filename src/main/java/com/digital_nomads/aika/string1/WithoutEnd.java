d3606f5c2be2e5590f90d7825da480493cf5ee9c
Aikanysh <mukanbet1986@gmail.com>
2024-05-20 23:18:32 +0600

    aika/String/withoutEnd

---
+++ src/main/java/com/digital_nomads/aika/string1/WithoutEnd.java
@@ -0,0 +1,10 @@
+package com.digital_nomads.aika.string1;
+
+public class WithoutEnd {
+    public String withoutEnd(String str) {
+        String mid = str.substring(1, str.length()-1);
+        return mid;
+
+    }
+
+}
