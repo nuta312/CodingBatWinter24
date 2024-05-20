d0ff9ba49ca608dfa947e0c5c37624b32d7156f7
Nargiza <nargizaknd210986@gmail.com>
2024-05-20 23:31:08 +0600

    test:SumDouble

---
+++ src/test/java/nargiza/string1_test/SumDoubleTest.java
@@ -0,0 +1,27 @@
+package nargiza.string1_test;
+
+import com.digital_nomads.nargiza.warmup1.SumDouble;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class SumDoubleTest {
+
+    SumDouble sumDouble = new SumDouble();
+
+@DataProvider(name = "word")
+    public Object[][] getSumDouble(){
+    return new Object[][]{
+            {1, 2, 3},
+            {3, 2, 5},
+            {2, 2, 8}
+    };
+}
+
+@Test(dataProvider = "word")
+    public void testSumDouble(int a, int b, int expected){
+    Assert.assertEquals(sumDouble.sumDouble(a, b), expected);
+}
+}
