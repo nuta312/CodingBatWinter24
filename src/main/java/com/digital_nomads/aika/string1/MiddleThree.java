4db14b8d3447b3f02822f33f53e10f36f0f45920
Aikanysh <mukanbet1986@gmail.com>
2024-05-21 00:42:46 +0600

    aika/String/middleThree

---
+++ src/main/java/com/digital_nomads/aika/string1/MiddleThree.java
@@ -0,0 +1,11 @@
+package com.digital_nomads.aika.string1;
+
+public class MiddleThree {
+    public String middleThree(String str) {
+        return str.substring(str.length()/2-1,str.length()/2+2);
+
+
+
+    }
+
+}
