b890f7064c34fcbf924a74458b4a9bf2a7708231
Aikanysh <mukanbet1986@gmail.com>
2024-05-20 23:50:17 +0600

    aika/String/left2

---
+++ src/main/java/com/digital_nomads/aika/string1/Left2.java
@@ -0,0 +1,10 @@
+package com.digital_nomads.aika.string1;
+
+public class Left2 {
+    public String left2(String str) {
+
+        String str1 = str.substring(2);
+        String str2 = str.substring(0,2);
+        return str1 +str2;
+    }
+}
