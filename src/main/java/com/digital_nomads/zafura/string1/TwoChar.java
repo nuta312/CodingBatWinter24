cff5faf540af4ffc49ca2916047e40619c22e5cb
zafura <zafura25@gmail.com>
2024-05-20 09:43:21 +0300

    twochar

---
+++ src/main/java/com/digital_nomads/zafura/string1/TwoChar.java
@@ -0,0 +1,16 @@
+package com.digital_nomads.zafura.string1;
+
+public class TwoChar {
+    public String twoChar(String str, int index) {
+        if(index < 0 || index >= str.length() - 1 ){
+            index = 0;
+        }
+        char [] i = new char [2];
+        i[0] = str.charAt(index);
+        i[1] = str.charAt(index +1 );
+
+        return new String(i);
+    }
+
+
+}
