2aee23bcabc15996250d1978bc00ff4b331eea12
Nargiza <nargizaknd210986@gmail.com>
2024-05-20 23:26:10 +0600

    test:MonkeyTrouble

---
+++ src/main/java/com/digital_nomads/nargiza/warmup1/MonkeyTrouble.java
@@ -0,0 +1,10 @@
+package com.digital_nomads.nargiza.warmup1;
+
+public class MonkeyTrouble {
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile == bSmile ) {
+            return true;
+        } return false;
+    }
+}
---
+++ src/test/java/nargiza/string1_test/MonkeyTroubleTest.java
@@ -0,0 +1,27 @@
+package nargiza.string1_test;
+
+import com.digital_nomads.nargiza.warmup1.MonkeyTrouble;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MonkeyTroubleTest {
+
+MonkeyTrouble monkeyTrouble = new MonkeyTrouble();
+
+@DataProvider(name = "word")
+public Object[][] getMonkeyTrouble(){
+    return new Object[][]{
+            {true, true, true},
+            {false, false, true},
+            {true, false, false}
+    };
+}
+
+@Test(dataProvider = "word")
+public void testMonkeyTrouble(boolean aSmile, boolean bSmile, boolean expected){
+    Assert.assertEquals(monkeyTrouble.monkeyTrouble(aSmile, bSmile), expected);
+}
+}
