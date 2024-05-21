bfdb248afd20cb13a6d74f62a3dc21cf0b0cc355
Aisuluu <kemelovaaisuluu@gmail.com>
2024-05-22 00:26:58 +0600

    Ais/MakeTags

---
+++ src/main/java/digital_nomads/aisuluu/string1/MakeTags.java
@@ -0,0 +1,7 @@
+package digital_nomads.aisuluu.string1;
+
+public class MakeTags {
+    public String makeTags (String tag, String word){
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+}
